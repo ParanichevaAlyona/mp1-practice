eb23fdb1bded95c35ae374a40657ee13db51ae9c
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2019-06-03 16:44:17 +0300

    Update Date.cpp

--- paranicheva_av/Practice/Practice9/Date.cpp
+++ paranicheva_av/Practice/Practice9/Date.cpp
@@ -10,9 +10,9 @@ Date::Date()
 }
 Date::Date(int _d, int _m, int _y)
 {	
-	y = Sety(_y);
-	m = Setm(_m);
-	d = Setd(_d, _m, _y);
+	this->d = _d;
+	this->m = _m;
+	this->y = _y;
 }
 Date::~Date()
 {
@@ -20,68 +20,18 @@ Date::~Date()
 	m = 0;
 	y = 0;
 }
-bool Date::Leapy(int _y)
-{
-	if (_y % 4 == 0)
-	{ 
-		if (_y % 100 == 0)
-		{
-			if (_y % 400 ==0)
-				return true;
-			return false;
-		}
-		return true;
-	}
-	return false;
-}
-int Date::Setd(int _d, int _m, int _y)
-{
-	int k[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	if (_m == 2)
-		if (Leapy(_y))
-			k[1] = 29;
-	if ((_d >= 1) && (_d <= k[_m - 1]))
-	d = _d;
-	else throw "The day is incorrect";
-	return _d;
-}
-int Date::Setm(int _m)
-{
-	if ((_m >= 1) && (_m <= 12))
-		m = _m;
-	else throw "The month is incorrect";
-	return _m;
-}
-int Date::Sety(int _y)
-{
-	if ((_y >= 1900) && (_y <= 2100))
-	y = _y;
-	else throw "The year is incorrect"; 
-	return _y;
-}
-int Date::Getd()
-{
-	return d;
-}
-int Date::Getm()
-{
-	return m;
-}
-int Date::Gety()
-{
-	return y;
-}
+
 void Date::Print()
 {
 	cout << d << "/" << m << "/" << y << endl;
 }
-bool Date::operator==(const Date& tmp)
+bool Date::operator==(const Date& tmp) const
 {
 	if ((d == tmp.d) && (m == tmp.m) && (y == tmp.y))
 		return true;
 	return false;
 }
-Date& Date::operator=(const Date date)
+const Date& Date::operator=(const Date date)
 {
 	d = date.d;
 	m = date.m;
