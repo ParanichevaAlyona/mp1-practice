9cdd9c5775e850fd22821873dc35acec27da7208
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2019-06-03 16:41:04 +0300

    Update Head.h

--- paranicheva_av/Practice/Practice8/Head.h
+++ paranicheva_av/Practice/Practice8/Head.h
@@ -6,14 +6,11 @@ class Matrix
 	double *m;
 	int rows, cols;
 public:
-	Matrix();
 	Matrix(int _rows, int _cols);
 	Matrix(double* _m, int _rows, int _cols);
 	Matrix(const Matrix& M);
 	~Matrix();
-	Matrix& Fill();
-	void Out();
-	Matrix operator= (const Matrix& M);
+	const Matrix operator= (const Matrix& M);
 	Matrix operator+ (const Matrix& M)const;
 	Matrix operator- (const Matrix& M)const;
 	Matrix operator* (const Matrix&)const;
@@ -22,6 +19,7 @@ public:
 	Matrix operator* (double a)const;
 	double* operator[] (int ind) const;
 	bool operator== (const Matrix& m) const;
-};
 
-#endif
+	friend istream& operator>> (istream& input, Matrix& M); 
+    	friend ostream& operator<< (ostream& output, const Matrix& M); 
+};
